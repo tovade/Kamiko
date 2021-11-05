c27678815c69b5b2b49819fc371b06787b75432d
tovade <gamingethan650@gmail.com>
2021-11-05 12:57:07 +0100

    feat(ChannelInfo): more info depending on chn type

--- src/commands/information/channelinfo.ts
+++ src/commands/information/channelinfo.ts
@@ -1,16 +1,23 @@
-import { ColorResolvable, MessageEmbed, TextChannel } from 'discord.js';
-const moment = require('moment');
-import Command from '../../structures/Command';
-import DiscordClient from '../../structures/DiscordClient';
-import { IContext } from '../../utils/interfaces';
+import { Channel, ColorResolvable, MessageEmbed, StageChannel, TextChannel, VoiceChannel } from 'discord.js'
+const moment = require('moment')
+import Command from '../../structures/Command'
+import DiscordClient from '../../structures/DiscordClient'
+import { IContext } from '../../utils/interfaces'
 
 const types = {
-    GUILD_TEXT: 'Text Channel',
-    GUILD_VOICE: 'Voice Channel',
+    GUILD_TEXT: '<:channel:905436084735979520> Text Channel',
+    DM: 'Direct Message',
+    GUILD_VOICE: '<:voice_channel:906131217773109248> Voice Channel',
     GUILD_CATEGORY: 'Category',
     GUILD_NEWS: 'News channel',
-    GUILD_STORE: 'Store channel'
-};
+    GUILD_STORE: 'Store channel',
+    GROUP_DM: 'Group Direct Message',
+    GUILD_NEWS_THREAD: 'News Thread',
+    GUILD_PUBLIC_THREAD: 'Public Thread',
+    GUILD_PRIVATE_THREAD: 'Private Thread',
+    GUILD_STAGE_VOICE: '<:stage_channel:906131217378840617> Stage Channel',
+    UNKNOWN: 'Unknown'
+}
 export default class ChannelInfoCommand extends Command {
     constructor(client: DiscordClient) {
         super(client, {
@@ -21,51 +28,137 @@ export default class ChannelInfoCommand extends Command {
             require: {
                 channel: true
             }
-        });
+        })
     }
     async run(ctx: IContext) {
-        const { channel } = ctx.mentions;
+        const channel = ctx.mentions.channel as Channel
 
-        const type = channel?.type;
+        const name = (channel as TextChannel)?.name
+        const type = channel?.type
 
-        const createdAt = moment(channel?.createdTimestamp).format('LLLL');
+        const createdAt = moment(channel?.createdTimestamp).format('LLLL')
 
-        const chn = channel as TextChannel;
+        let embed
+        if (channel?.type === 'GUILD_TEXT') {
+            const chn = channel as TextChannel
 
-        const name = chn.name;
-
-        const nsfw = chn.nsfw ? 'Yes' : 'No';
-
-        const embed = new MessageEmbed({
-            title: `<:channel:905436084735979520> ${name}`,
-            color: this.client.config.color,
-            fields: [
-                {
-                    name: '**ID:**',
-                    value: channel?.id as string,
-                    inline: true
-                },
-                {
-                    name: '**Type:**',
-                    value: type as string,
-                    inline: true
-                },
-                {
-                    name: '**Created at:**',
-                    value: createdAt as string,
-                    inline: true
-                },
-                { name: '**Topic:**', value: (chn.topic as string) || 'No topic', inline: true },
-                { name: '**Nsfw:**', value: nsfw as string, inline: true },
-                { name: '**Category:**', value: (chn.parent?.name as string) || 'No category', inline: true },
-                { name: '**Slowmode:**', value: (chn.rateLimitPerUser as unknown as string) || 'No slowmode', inline: true }
-            ],
-            footer: {
-                text: '© Kamiko'
-            }
-        });
+            const nsfw = chn.nsfw ? 'Yes' : 'No'
+            embed = new MessageEmbed({
+                title: `${name}`,
+                color: this.client.config.color,
+                fields: [
+                    {
+                        name: '**ID:**',
+                        value: channel?.id as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Type:**',
+                        value: types[type] as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Created at:**',
+                        value: createdAt as string,
+                        inline: true
+                    },
+                    { name: '**Topic:**', value: (chn.topic as string) || 'No topic', inline: true },
+                    { name: '**Nsfw:**', value: nsfw as string, inline: true },
+                    { name: '**Category:**', value: (chn.parent?.name as string) || 'No category', inline: true },
+                    { name: '**Slowmode:**', value: (chn.rateLimitPerUser as unknown as string) || 'No slowmode', inline: true }
+                ],
+                footer: {
+                    text: '© Kamiko'
+                }
+            })
+        } else if (channel.type === 'GUILD_VOICE') {
+            const chn = channel as VoiceChannel
+            embed = new MessageEmbed({
+                title: `${name}`,
+                color: this.client.config.color,
+                fields: [
+                    {
+                        name: '**ID:**',
+                        value: channel?.id as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Type:**',
+                        value: types[type] as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Created at:**',
+                        value: createdAt as string,
+                        inline: true
+                    },
+                    { name: '**Category:**', value: (chn.parent?.name as string) || 'No category', inline: true },
+                    { name: '**User Limit:**', value: chn.userLimit.toString() || 'No limit', inline: true },
+                    { name: '**Bit Rate:**', value: `${chn.bitrate.toString()}kbps`, inline: true }
+                ],
+                footer: {
+                    text: '© Kamiko'
+                }
+            })
+        } else if (channel.type === 'GUILD_STAGE_VOICE') {
+            const chn = channel as StageChannel
+            embed = new MessageEmbed({
+                title: `${name}`,
+                color: this.client.config.color,
+                fields: [
+                    {
+                        name: '**ID:**',
+                        value: channel?.id as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Type:**',
+                        value: types[type] as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Created at:**',
+                        value: createdAt as string,
+                        inline: true
+                    },
+                    { name: '**Category:**', value: (chn.parent?.name as string) || 'No category', inline: true },
+                    { name: '**Bit Rate:**', value: `${chn.bitrate.toString()}kbps`, inline: true },
+                    { name: '**Channel Members:**', value: `${chn.members.size.toString()}kbps`, inline: true }
+                ],
+                footer: {
+                    text: '© Kamiko'
+                }
+            })
+        } else {
+            const chn = channel as TextChannel
+            embed = new MessageEmbed({
+                title: `${name}`,
+                color: this.client.config.color,
+                fields: [
+                    {
+                        name: '**ID:**',
+                        value: channel?.id as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Type:**',
+                        value: types[type] as string,
+                        inline: true
+                    },
+                    {
+                        name: '**Created at:**',
+                        value: createdAt as string,
+                        inline: true
+                    },
+                    { name: '**Category:**', value: (chn.parent?.name as string) || 'No category', inline: true }
+                ],
+                footer: {
+                    text: '© Kamiko'
+                }
+            })
+        }
         await ctx.message.channel.send({
-            embeds: [embed]
-        });
+            embeds: [embed as MessageEmbed]
+        })
     }
 }
